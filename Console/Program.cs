2412c27fc668716723b116a2e409e980afae2680
vijay <87221187+vijay-pilla@users.noreply.github.com>
2024-12-15 20:46:46 +0530

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,1 +1,119 @@
-﻿
+[
+  {
+    "id": 0,
+    "name": "cooked rice",
+    "protein": 2.7,
+    "carbs": 28,
+    "fibre": 0.4,
+    "fat": 0.3,
+    "totalCalories": 130
+  },
+{
+    "id": 0,
+    "name": "Gauva",
+    "protein": 2.6,
+    "carbs": 14,
+    "fibre": 5,
+    "fat": 1,
+    "totalCalories": 68
+  },
+{
+    "id": 0,
+    "name": "Badam",
+    "protein": 21,
+    "carbs": 22,
+    "fibre": 12,
+    "fat": 49,
+    "totalCalories": 576
+  },
+{
+    "id": 0,
+    "name": "Cashews",
+    "protein": 18.22,
+    "carbs": 30.19,
+    "fibre": 3.30,
+    "fat": 43.85,
+    "totalCalories": 553
+  },
+{
+    "id": 0,
+    "name": "pumpkin seeds",
+    "protein": 24.54,
+    "carbs": 17.81,
+    "fibre": 3.9,
+    "fat": 45.85,
+    "totalCalories": 541
+  },
+{
+    "id": 0,
+    "name": "sunflower seeds",
+    "protein": 22.78,
+    "carbs": 18.76,
+    "fibre": 10.5,
+    "fat": 49.57,
+    "totalCalories": 570
+  },
+{
+    "id": 0,
+    "name": "flax seeds",
+    "protein": 18,
+    "carbs": 29,
+    "fibre": 27,
+    "fat": 42,
+    "totalCalories": 534
+  },
+{
+    "id": 0,
+    "name": "chia seeds",
+    "protein": 17,
+    "carbs": 42,
+    "fibre": 34,
+    "fat": 31,
+    "totalCalories": 486
+  },
+{
+    "id": 0,
+    "name": "sweet potato",
+    "protein": 1.6,
+    "carbs": 20,
+    "fibre": 3,
+    "fat": 0.1,
+    "totalCalories": 86
+  },
+{
+    "id": 0,
+    "name": "whole eggs",
+    "protein": 12,
+    "carbs": 1.1,
+    "fibre": 0,
+    "fat": 11,
+    "totalCalories": 155
+  },
+{
+    "id": 0,
+    "name": "ON whey protein",
+    "protein": 70,
+    "carbs": 15.5,
+    "fibre": 0,
+    "fat": 5.5,
+    "totalCalories": 391.5
+  },
+{
+    "id": 0,
+    "name": "Keera",
+    "protein": 0.7,
+    "carbs": 3.6,
+    "fibre": 0.5,
+    "fat": 0.1,
+    "totalCalories": 15
+  },
+{
+    "id": 0,
+    "name": "watermelon",
+    "protein": 0.6,
+    "carbs": 8,
+    "fibre": 0.4,
+    "fat": 0.2,
+    "totalCalories": 30
+  }
+]﻿
